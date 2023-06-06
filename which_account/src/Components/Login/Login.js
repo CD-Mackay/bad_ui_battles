2f3e47cc9b024ba1aa4a63bfed376c24626ddaa8
Connor Mackay <connor.mackay@gmail.com>
2023-06-06 12:50:53 -0400

    implement login function

--- which_account/src/Components/Login/Login.js
+++ which_account/src/Components/Login/Login.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = ({accounts}) => {
 
+  const [password, setPassword] = useState()
+
+  const handleChange = (e) => {
+    setPassword(e.target.value);
+  };
+
   const handleLogin = (e) => {
     e.preventDefault();
+    let possibleAccounts = accounts.filter((element) => element.password === password)
+    console.log(possibleAccounts)
+    return possibleAccounts;
   }
   return (
     <form>
-      <input type="password" placeholder="enter password"></input>
+      <input type="password" placeholder="enter password" onChange={(e) => handleChange(e)}></input>
       <button onClick={(e) => handleLogin(e)}>Login</button>
     </form>
   )
