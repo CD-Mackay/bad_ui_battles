e9d6c16c29ece5c91d7044e8b1058e487bad6d22
Connor Mackay <connor.mackay@gmail.com>
2023-06-07 13:08:25 -0400

    style accounts page

--- which_account/src/Components/Accounts/Accounts.js
+++ which_account/src/Components/Accounts/Accounts.js
@@ -1,15 +1,21 @@
 import React from "react";
-import './Accounts.css';
-
+import "./Accounts.css";
 
 const Accounts = ({ accounts }) => {
-
-  const AccountItem = ({account}) => {
-    return <p>{account.username}</p>;
+  const AccountItem = ({ account }) => {
+    return (
+      <div className="account-item">
+        <p>{account.username}</p>
+        <button className="account-button">Login!</button>
+      </div>
+    );
   };
   return (
     <div>
-      <p>Looks like that password belongs to a few accounts, please select which one is yours (Honour system)</p>
+      <h3>
+        Looks like that password belongs to a few accounts, please select which
+        one is yours (Honour system)
+      </h3>
       {accounts.map((element, index) => {
         return <AccountItem account={element} key={index} />;
       })}
