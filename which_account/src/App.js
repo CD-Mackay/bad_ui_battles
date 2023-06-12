a0a6f46ba40f28755bbf482ba010568d1477c797
Connor Mackay <connor.mackay@gmail.com>
2023-06-12 17:49:03 -0400

    add submit handler to app

--- which_account/src/App.js
+++ which_account/src/App.js
@@ -1,53 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './Components/Login/Login';
-import Accounts from './Components/Accounts/Accounts';
-import { useState } from 'react';
-import Reset from './Components/Reset/Reset';
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./Components/Login/Login";
+import Accounts from "./Components/Accounts/Accounts";
+import { useState, useRef } from "react";
+import Reset from "./Components/Reset/Reset";
+import handleSubmit from "./handles/handles";
 
 function App() {
-
-  const [possible, setPossible] = useState([])
+  const [possible, setPossible] = useState([]);
 
   const accounts = [
     {
       username: "Dennis@paddyspub.com",
-      password: "password"
+      password: "password",
     },
     {
       username: "Charlie@paddyspub.com",
-      password: "pass"
+      password: "pass",
     },
     {
       username: "Frank@paddyspub.com",
-      password: "pass"
+      password: "pass",
     },
     {
       username: "Dee@paddyspub.com",
-      password: "password"
+      password: "password",
     },
     {
       username: "Mac@paddyspub.com",
-      password: "password"
+      password: "password",
     },
   ];
 
+  const dataRef = useRef();
+
   const handleLogin = (e, pass) => {
     e.preventDefault();
-    let possibleAccounts = accounts.filter((element) => element.password === pass)
+    let possibleAccounts = accounts.filter(
+      (element) => element.password === pass
+    );
     setPossible(possibleAccounts);
     setView("accounts");
     return possibleAccounts;
-  }
+  };
 
   const [view, setView] = useState("login");
 
+  const submithandler = (e) => {
+    e.preventDefault()
+    handleSubmit(dataRef.current.value)
+    dataRef.current.value = ""
+  }
+
   return (
     <div className="App">
-      {view === "login" && <Login accounts={accounts} handleLogin={handleLogin} />}
-      {view === "accounts" && <Accounts accounts={possible} allAccounts ={accounts} setView={setView} />}
-      {view === "success" && <h2>You are now logged in!</h2>}
-      {view === "reset" && <Reset setView={setView} />}
+       <form onSubmit={submithandler}>
+        <input type= "text" ref={dataRef} />
+        <button type = "submit">Save</button>
+      </form>
+        {view === "login" && (
+          <Login accounts={accounts} handleLogin={handleLogin} />
+        )}
+        {view === "accounts" && (
+          <Accounts
+            accounts={possible}
+            allAccounts={accounts}
+            setView={setView}
+          />
+        )}
+        {view === "success" && <h2>You are now logged in!</h2>}
+        {view === "reset" && <Reset setView={setView} />}
     </div>
   );
 }
