112ef2eb78c7992a9602cd9bceb9c1bcc4aaff44
Connor Mackay <connor.mackay@gmail.com>
2023-06-12 17:49:20 -0400

    build submit handler

---
+++ which_account/src/handles/handles.js
@@ -0,0 +1,17 @@
+import { addDoc, collection} from '@firebase/firestore';
+import { firestore } from '../firebase_setup/firebase';
+
+const handleSubmit = (testData) => {
+  const ref = collection(firestore, "test_data");
+
+  let data = {
+    testData: testData
+  }
+  try {
+    addDoc(ref, data)
+  } catch(err){
+    console.log(err)
+  }
+};
+
+export default handleSubmit;
