0ed0444c63da651537f41b3567f0dd9d4ac3a5c3
Connor Mackay <connor.mackay@gmail.com>
2023-06-13 13:54:10 -0400

    implement reset password method

--- which_account/src/Components/Reset/Reset.js
+++ which_account/src/Components/Reset/Reset.js
@@ -1,12 +1,25 @@
-import React from "react";
+import { updateDoc } from "firebase/firestore";
+import React, { useRef } from "react";
 import "./Reset.css";
+import { firestore } from "../../firebase_setup/firebase";
+import { doc } from "firebase/firestore";
 
 const Reset = ({setView}) => {
+
+  const passRef = useRef();
+
+  const handleReset = async (e) => {
+    e.preventDefault();
+    let accountRef = doc(firestore, "users", id);
+    await updateDoc(accountRef, {
+      password: passRef.current.value
+    })
+  }
+
   return (
     <div className="reset">
       <input type="password" placeholder="new password"></input>
-      <input type="password" placeholder="new password"></input>
-      <button onClick={() => setView("success")}>Reset!</button>
+      <button onClick={(e) => handleReset(e)}>Reset!</button>
     </div>
   );
 };
