9ab3991a0ddbd06ccb27596bd8fee744b7d93d51
Connor Mackay <connor.mackay@gmail.com>
2023-06-14 16:13:07 -0400

    implement handlechange fr color picker

--- color-picker/src/App.js
+++ color-picker/src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 import { ChromePicker } from "react-color";
 
 function App() {
-  const [color, setColor] = useState("");
+  const [color, setColor] = useState({ r: 0, g: 0, b: 0 });
+  const [date, setDate] = useState({ day: 0, month: 0, year: 0 });
 
-  const handleChangeColor = (color) => {
-    console.log(color.rgb);
-    setColor(color.hex);
+  const handleChangeColor = (colour) => {
+    setColor({ r: colour.rgb.r, g: colour.rgb.g, b: colour.rgb.b });
+    setDate({ day: colour.rgb.r, month: colour.rgb.g, year: colour.rgb.b });
   };
   return (
     <div className="App">
-      <ChromePicker onChangeComplete={handleChangeColor}
-      color={color}
-       />
+      <ChromePicker onChangeComplete={handleChangeColor} color={color} />
+      <p>{color.r.toString().slice(0, 2)}</p>
+      <p>{color.g.toString().slice(0, 2)}</p>
+      <p>{color.b.toString().slice(0, 2)}</p>
     </div>
   );
 }
