c43a36df771a307c2b59004505273dbc2a047d47
Connor Mackay <connor.mackay@gmail.com>
2023-06-16 12:26:52 -0400

    add onchange function

--- ddos-proof-auth/src/App.js
+++ ddos-proof-auth/src/App.js
@@ -1,13 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const handleChange = () => {
+    inputArr.sort(() => (Math.random() > 0.5 ? 1 : -1));
+    console.log(inputArr);
+  };
+
+  const inputArr = [
+    <input type="text" placeholder="1" onChange={handleChange} key={1} />,
+    <input type="password" placeholder="2" onChange={handleChange} key={2} />,
+    <input type="password" placeholder="3" onChange={handleChange} key={3} />,
+  ];
+
   return (
     <div className="App">
       <form className="login-form">
-        <input type="text" />
-        <input type="password" />
-        <input type="password" />
+        {inputArr.map((element) => {
+          return element;
+        })}
       </form>
     </div>
   );
