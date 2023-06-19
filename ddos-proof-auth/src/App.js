40de166cad7a274b66694a6b2c84c5638f229c66
Connor Mackay <connor.mackay@gmail.com>
2023-06-19 15:09:31 -0400

    refactor inputs array data

--- ddos-proof-auth/src/App.js
+++ ddos-proof-auth/src/App.js
@@ -1,23 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const handleChange = () => {
+  const [inputValue, setInputValue] = useState({
+    username: "",
+    password: "",
+    passConfirm: "",
+  });
+  const handleChange = (e) => {
+    inputArr.forEach((element) => console.log(element.key));
     inputArr.sort(() => (Math.random() > 0.5 ? 1 : -1));
+    inputArr.forEach((element) => console.log(element.key));
   };
-
   const inputArr = [
-    <input type="text" placeholder="1" onChange={handleChange} key={1} />,
-    <input type="password" placeholder="2" onChange={handleChange} key={2} />,
-    <input type="password" placeholder="3" onChange={handleChange} key={3} />,
+    {
+      type: "text",
+      key: 1,
+    },
+    {
+      type: "password",
+      key: 2,
+    },
+    {
+      type: "password",
+      key: 3,
+    },
   ];
+
+  const inputs = inputArr.map((element) => {
+    return (
+      <input type={element.type} key={element.key} placeholder={element.key} onChange={(e) => handleChange(e)} />
+    );
+  });
 
   return (
     <div className="App">
-      <form className="login-form">
-        {inputArr.map((element) => {
-          return element;
-        })}
-      </form>
+      <form className="login-form">{inputs}</form>
     </div>
   );
 }
