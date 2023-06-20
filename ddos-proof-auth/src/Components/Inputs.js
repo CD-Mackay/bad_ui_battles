08be7336928681ce5488675651cf453148284664
Connor Mackay <connor.mackay@gmail.com>
2023-06-20 12:59:11 -0400

    add labels to inputs

--- ddos-proof-auth/src/Components/Inputs.js
+++ ddos-proof-auth/src/Components/Inputs.js
@@ -1,28 +1,26 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import './Inputs.css';
+import { render } from "@testing-library/react";
+import React from "react";
+import "./Inputs.css";
 
-const Inputs = ({inputs, handleChange}) => {
-
+const Inputs = ({ inputs, handleChange }) => {
   const renderInputs = () => {
     return inputs.map((element) => {
       console.log(element.key);
       return (
-        <input
-          type={element.field}
-          key={element.key}
-          placeholder={element.key}
-          onChange={(e) => handleChange(e)}
-        />
+        <>
+          <label>{element.field}</label>
+          <input
+            type={element.field}
+            key={element.key}
+            placeholder={element.key}
+            onChange={(e) => handleChange(e)}
+          />
+        </>
       );
     });
   };
 
-  return (
-    <form className='input-form'>
-      {renderInputs()}
-    </form>
-  )
+  return <form className="input-form">{renderInputs()}</form>;
 };
 
 export default Inputs;
