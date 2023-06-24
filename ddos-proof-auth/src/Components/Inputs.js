753236287ee36071c12c1ad9e2dae9002920425b
Connor Mackay <connor.mackay@gmail.com>
2023-06-24 18:04:26 -0400

    style input layout

--- ddos-proof-auth/src/Components/Inputs.js
+++ ddos-proof-auth/src/Components/Inputs.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Inputs.css";
 
-const Input = ({ type, name, onChange, inputRef, inputValue }) => {
+const Input = ({ type, name, onChange, inputRef, inputValue, label }) => {
   return (
     <div className="input-wrapper">
-      <label htmlFor={name}>{name}</label>
+      <label htmlFor={name}>{label}</label>
       <input
         onChange={onChange}
         type={type}
