d1d4e2589215a795cb7c5a22454efd1134871877
Connor Mackay <connor.mackay@gmail.com>
2023-06-27 16:22:46 -0400

    implement onchange for input

--- phone-slider/src/App.js
+++ phone-slider/src/App.js
@@ -4,9 +4,14 @@ import { useState } from 'react';
 function App() {
 
   const [phone, setPhone] = useState(5555555555)
+
+  const handleChange = (e) => {
+    setPhone(e.target.value)
+  }
   return (
     <div className="App">
-      <input type="range" min="1111111111" max="9999999999"/>
+      <input type="range" min="1111111111" value={phone} onChange={(e) => handleChange(e)}max="9999999999" />
+      <p>{phone}</p>
     </div>
   );
 }
