6dae8b013a4bdee83b5f510e36e3aa7a773f1cb4
Connor Mackay <connor.mackay@gmail.com>
2023-06-27 16:21:14 -0400

    Add slider input and state

--- phone-slider/src/App.js
+++ phone-slider/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [phone, setPhone] = useState(5555555555)
   return (
     <div className="App">
+      <input type="range" min="1111111111" max="9999999999"/>
     </div>
   );
 }
