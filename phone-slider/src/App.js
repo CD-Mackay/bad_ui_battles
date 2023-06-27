84877194b622dc408a80331d99818bf460fc5c3f
Connor Mackay <connor.mackay@gmail.com>
2023-06-27 16:26:29 -0400

    Add README

--- phone-slider/src/App.js
+++ phone-slider/src/App.js
@@ -1,18 +1,24 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-
-  const [phone, setPhone] = useState(5555555555)
+  const [phone, setPhone] = useState(5555555555);
 
   const handleChange = (e) => {
-    setPhone(e.target.value)
-  }
+    setPhone(e.target.value);
+  };
+
   return (
     <div className="App">
-      <input type="range" min="1111111111" value={phone} onChange={(e) => handleChange(e)}max="9999999999" />
-      <span>({phone.toString().slice(0,3)})</span>
-      <span>-{phone.toString().slice(3,6)}</span>
+      <input
+        type="range"
+        min="1111111111"
+        value={phone}
+        onChange={(e) => handleChange(e)}
+        max="9999999999"
+      />
+      <span>({phone.toString().slice(0, 3)})</span>
+      <span>-{phone.toString().slice(3, 6)}</span>
       <span>-{phone.toString().slice(7)}</span>
     </div>
   );
