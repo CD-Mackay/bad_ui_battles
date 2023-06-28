37bd05db1e3580eeb617c6a48fc7ffee67d180fe
Connor Mackay <connor.mackay@gmail.com>
2023-06-28 15:23:53 -0400

    add inputs and textarea

--- morse-code-input/src/App.js
+++ morse-code-input/src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <textarea disabled>random stuff!!!</textarea>
+      <div className="button-wrapper">
+        <button>.</button>
+        <button>-</button>
+        <button>Enter</button>
+      </div>
     </div>
   );
 }
