35e08e90b008753548b03761743239ffdc441208
Connor Mackay <connor.mackay@gmail.com>
2023-07-05 19:19:57 -0400

    style translation chart

--- morse-code-input/src/Components/MorseChart/MorseChart.js
+++ morse-code-input/src/Components/MorseChart/MorseChart.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./MorseChart.css";
 
-const MorseChart = () => {
-  return <p>I am morsechart</p>;
+const MorseChart = ({ data }) => {
+  return (
+    <div className="morse-chart">
+      {data.map((element, index) => {
+        return (
+          <span className="morse-chart-element" key={index}>
+            {element[0]}<strong>{element[1]}</strong>
+          </span>
+        );
+      })}
+    </div>
+  );
 };
 
 export default MorseChart;
