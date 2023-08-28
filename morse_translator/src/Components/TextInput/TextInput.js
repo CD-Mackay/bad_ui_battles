cd9771be7608d768d4fa15b739c56570bc8e41af
Connor Mackay <connor.mackay@gmail.com>
2023-08-28 11:43:27 -0400

    build textinput component

---
+++ morse_translator/src/Components/TextInput/TextInput.js
@@ -0,0 +1,13 @@
+import './TextInput.css';
+
+function TextInput() {
+  return (
+    <div>
+      <textarea placeholder="Enter your message here">
+
+      </textarea>
+    </div>
+  );
+}
+
+export default TextInput;
