985561607fdd4a38ce149cfc46a00523141bd63b
Connor Mackay <connor.mackay@gmail.com>
2023-09-01 12:22:07 -0400

    implement base version of password validation

--- bad_password_validation/src/App.js
+++ bad_password_validation/src/App.js
@@ -7,12 +7,19 @@ function App() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState([{string: "password must contain a number", completed: false}])
   const validatePass = () => {
-
+    if (/\d/.test(password)) {
+      let errorArr = [...error];
+      errorArr[0].completed = true;
+      errorArr.push({string: "password must contain the word robot", completed: false});
+      console.log(errorArr);
+      setError(errorArr);
+    }
   }
 
   const handleInput = (e) => {
     const { value } = e.target;
     setPassword(value);
+    validatePass()
   }
   return (
     <div className="App">
