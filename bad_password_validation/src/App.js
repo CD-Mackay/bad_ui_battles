1af0afb8d3eb867374f83427856b8c2f6e18999d
Connor Mackay <connor.mackay@gmail.com>
2023-09-01 12:13:52 -0400

    add state and onchange handlers to form

--- bad_password_validation/src/App.js
+++ bad_password_validation/src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [password, setPassword] = useState("");
+  const validatePass = () => {
+
+  }
+
+  const handleInput = (e) => {
+    const { value } = e.target;
+    setPassword(value);
+  }
   return (
     <div className="App">
       <form className="user-form">
         <input type="text"></input>
-        <input type="password"></input>
+        <input type="password" value={password} onChange={(e) => handleInput(e)}></input>
       </form>
     </div>
   );
