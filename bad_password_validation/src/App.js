f4df0d9608e6af7ca01cfab3e82a771578665ad0
Connor Mackay <connor.mackay@gmail.com>
2023-09-09 15:30:41 -0400

    Refactor render error list method

--- bad_password_validation/src/App.js
+++ bad_password_validation/src/App.js
@@ -5,77 +5,58 @@ import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
 
 function App() {
   const [password, setPassword] = useState("");
-  const [error, setError] = useState([
-    { string: "password must contain a number", completed: false },
-  ]);
-  const validatePass = (value) => { // Too much randomness in passwords, need to find a way to only complete the validation step
-    let errorArr = [...error];
-    if (/\d/.test(value) && error.length < 2) {
-      errorArr[0].completed = true;
+  const [error, setError] = useState({
+    "password must contain a number": false,
+  });
+  const validatePass = (value) => {
+    // Too much randomness in passwords, need to find a way to only complete the validation step
+    let errorObj = { ...error };
+    if (/\d/.test(value)) {
+      errorObj["password must contain a number"] = true;
+      errorObj["password must include the word robot"] = false;
     }
-    if (/\d/.test(value) && error.length < 3) {
-      errorArr[1].completed = true;
+
+    if (value.includes("robot")) {
+      errorObj["password must include the word robot"] = true;
+      errorObj["password must contain the phrase zoop"] = false;
     }
-    if (!value.includes("robot") && error.length < 2) {
-      errorArr.splice(0, 0, {
-        string: "password must contain the word robot",
-        completed: false,
-      });
+    if (value.includes("zoop")) {
+      errorObj["password must contain the phrase zoop"] = true;
+      errorObj[
+        "after you slide to the left, you must slide to the ______"
+      ] = false;
     }
-    if (
-      value.includes("robot") &&
-      !value.includes("zoop") &&
-      error.length < 3
-    ) {
-      errorArr[0].completed = true;
-      errorArr.splice(0, 0, {
-        string: "password must contain zoop",
-        completed: false,
-      });
+    if (value.includes("right")) {
+      errorObj[
+        "after you slide to the left, you must slide to the ______"
+      ] = true;
+      errorObj["password must contain zz"] = false;
     }
-    if (
-      value.includes("zoop") &&
-      !value.includes("right") &&
-      error.length < 4
-    ) {
-      errorArr[0].completed = true;
-      errorArr.splice(0, 0, {
-        string: "After you slide to the left, you must slide to the _____",
-        completed: false,
-      });
+    if (value.includes("zz")) {
+      errorObj["password must contain zz"] = true;
+      errorObj["press f to pay respects"] = false;
     }
-    if (value.includes("right") && error.length < 5) {
-      errorArr[0].completed = true;
-      errorArr.splice(0, 0, {
-        string: "must include the letter z twice",
-        completed: false,
-      });
+    if (value.includes("f")) {
+      errorObj["press f to pay respects"] = true;
     }
-    if (value.includes("zz") && error.length < 6) {
-      errorArr[0].completed = true;
-      errorArr.splice(0, 0, {
-        string: "press f to pay respects",
-        completed: false,
-      });
-    }
-    if (value.includes("f") && error.length < 7) {
-      errorArr[0].completed = true;
-    }
-    console.log(errorArr);
-    setError(errorArr);
+    console.log(errorObj);
+    setError(errorObj);
   };
 
   const showReqs = () => {
-    return error.map((element, index) => {
-      return (
+    let array = [];
+    Object.keys(error).forEach(function (key, index) {
+      array.push(
         <li
-          key={index}
-          className={element.completed ? "complete" : "incomplete"}
+          index={index}
+          className={error[key] === true ? "complete" : "incomplete"}
         >
-          {element.string}
-          {element.completed ? <AiOutlineCheck color="green" /> : <AiOutlineClose color="red" />}
+          {key}
         </li>
       );
+    });
+    return array.map((element) => {
+      return element;
     });
   };
 
