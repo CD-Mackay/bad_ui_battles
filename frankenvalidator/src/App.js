49ec3787d0a87f81a728a3f21fc16086ce5d043d
Connor Mackay <connor.mackay@gmail.com>
2023-09-10 19:21:16 -0400

    import ddos protection components

--- frankenvalidator/src/App.js
+++ frankenvalidator/src/App.js
@@ -1,8 +1,96 @@
 import './App.css';
+import { useEffect, useState, useRef} from 'react';
+import Input from './Components/Input/Input';
 
 function App() {
+
+  const [inputValue, setInputValue] = useState({
+    username: "",
+    password: "",
+    passConfirm: "",
+  });
+
+  const [view, setView] = useState("login");
+
+  const inputRef = useRef(null);
+  const hiddenInput = document.getElementById("hidden-input");
+
+  const unFocus = () => {
+    // inputRef.current.blur();
+    console.log(hiddenInput);
+    hiddenInput.focus();
+  };
+
+  let inputArr = [
+    {
+      type: "text",
+      key: 1,
+      field: "username",
+      label: "username",
+    },
+    {
+      type: "password",
+      key: 2,
+      field: "password",
+      label: "password",
+    },
+    {
+      type: "password",
+      key: 3,
+      field: "passConfirm",
+      label: "confirm password",
+    },
+  ];
+
+  const [inputs, setInputs] = useState(inputArr);
+
+  const shuffleInputs = (array) => {
+    const newArr = [...array];
+    for (let i = newArr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+    }
+    return newArr;
+  };
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { value, name } = e.target;
+    console.log("value", value, "name", name);
+    setInputValue((prevNote) => ({
+      ...prevNote,
+      [name]: value,
+    }));
+  };
+
+  const handleLogin = () => {
+    if (
+      inputValue.password === inputValue.passConfirm &&
+      inputValue.password !== ""
+    ) {
+      setView("success");
+    }
+  };
+
+  useEffect(() => {
+    const mountArray = shuffleInputs(inputArr);
+    setInputs(mountArray);
+  }, []);
+
   return (
     <div className="App">
+        {inputs.map((element) => {
+            return (
+              <Input
+                type={element.type}
+                inputRef={inputRef}
+                onChange={(e) => handleShuffle(e)}
+                key={element.key}
+                name={element.field}
+                inputValue={inputValue}
+                label={element.label}
+              />
+            );
+          })}
     </div>
   );
 }
---
+++ frankenvalidator/src/Components/Input/Input.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Inputs.css";
+
+const Input = ({ type, name, onChange, inputRef, inputValue, label }) => {
+  return (
+    <div className="input-wrapper">
+      <label htmlFor={name}>{label}</label>
+      <input
+        onChange={onChange}
+        type={type}
+        id={name}
+        value={inputValue.name}
+        name={name}
+        ref={inputRef}
+        placeholder={label}
+      />
+    </div>
+  );
+};
+
+export default Input;
